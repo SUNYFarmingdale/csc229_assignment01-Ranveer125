c53dcd91e1d31d42e21d98dd85debcce9d3f5226
ranveersingh <ranveersingh@ranveers-air.lan>
2023-02-01 16:37:41 -0500

    Create getters and setters for data members

--- src/main/java/com/mycompany/csc229_assignment01/Course.java
+++ src/main/java/com/mycompany/csc229_assignment01/Course.java
@@ -33,5 +33,34 @@ package com.mycompany.csc229_assignment01;
  * @author MoaathAlrajab
  */
 public class Course {
+    
+    private int ID;
+    private String Name;
+    private String Code;
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public void setCode(String Code) {
+        this.Code = Code;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public String getCode() {
+        return Code;
+    }
+
 
 }
